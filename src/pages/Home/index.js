4b10ba588d899abef164dc94e3c5cb6a3393067f
LeoPFreitas <leonardo.cadu2@hotmail.com>
2020-03-23 20:59:09 -0300

    (style) add hp and header style

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,89 @@
 import React from 'react';
+import { MdAddShoppingCart } from 'react-icons/md';
 
-// import { Container } from './styles';
+import { ProductList } from './styles';
 
 export default function Home() {
-  return <div />;
+  return (
+    <ProductList>
+      <li>
+        <img src="https://via.placeholder.com/250.png" alt="Tênis" />
+        <strong>Descricao da imagem</strong>
+        <span>R$ 345,56</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img src="https://via.placeholder.com/250.png" alt="Tênis" />
+        <strong>Descricao da imagem</strong>
+        <span>R$ 345,56</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img src="https://via.placeholder.com/250.png" alt="Tênis" />
+        <strong>Descricao da imagem</strong>
+        <span>R$ 345,56</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img src="https://via.placeholder.com/250.png" alt="Tênis" />
+        <strong>Descricao da imagem</strong>
+        <span>R$ 345,56</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img src="https://via.placeholder.com/250.png" alt="Tênis" />
+        <strong>Descricao da imagem</strong>
+        <span>R$ 345,56</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img src="https://via.placeholder.com/250.png" alt="Tênis" />
+        <strong>Descricao da imagem</strong>
+        <span>R$ 345,56</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+    </ProductList>
+  );
 }
