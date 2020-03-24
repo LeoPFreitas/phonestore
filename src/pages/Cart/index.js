9155f9323dc1324bb97d0e75c226a9b3567b785c
LeoPFreitas <leonardo.cadu2@hotmail.com>
2020-03-24 18:28:23 -0300

    (style) add cart style

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,7 +1,69 @@
 import React from 'react';
+import {
+  MdRemoveCircleOutline,
+  MdAddCircleOutline,
+  MdDelete,
+} from 'react-icons/md';
 
-// import { Container } from './styles';
+import { Container, ProductTable, Total } from './styles';
 
 export default function Cart() {
-  return <div />;
+  return (
+    <Container>
+      <ProductTable>
+        <thead>
+          <tr>
+            <th />
+            <th>PRODUTO</th>
+            <th>QTD</th>
+            <th>SUBTOTAL</th>
+            <th />
+          </tr>
+        </thead>
+
+        <tbody>
+          <tr>
+            <td>
+              <img
+                src="https://via.placeholder.com/250.png"
+                alt="Imagem placeholder"
+              />
+            </td>
+            <td>
+              <strong>Celular X</strong>
+              <span>R$123,90</span>
+            </td>
+            <td>
+              <div>
+                <button type="button">
+                  <MdRemoveCircleOutline size={20} color="#7159c1" />
+                </button>
+                <input type="number" readOnly value={1} />
+                <button type="button">
+                  <MdAddCircleOutline size={20} color="7159c1" />
+                </button>
+              </div>
+            </td>
+            <td>
+              <strong>R$234,90</strong>
+            </td>
+            <td>
+              <button type="button">
+                <MdDelete size={20} color="#7159c1" />
+              </button>
+            </td>
+          </tr>
+        </tbody>
+      </ProductTable>
+
+      <footer>
+        <button type="button">Finalizar pedido</button>
+
+        <Total>
+          <span>TOTAL</span>
+          <strong>R$174,90</strong>
+        </Total>
+      </footer>
+    </Container>
+  );
 }
